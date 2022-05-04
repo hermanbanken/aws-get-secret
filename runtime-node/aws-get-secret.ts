b255003bd08b6254fa80a4e5a06c54a8fa5586ae
Herman <herman@q42.nl>
2022-05-04 13:12:30 +0200

    v1.0.9

--- runtime-node/aws-get-secret.ts
+++ runtime-node/aws-get-secret.ts
@@ -6,31 +6,32 @@ import { findAll } from "./env";
 import { execSync } from "child_process";
 
 const [args, remainder] = parse(process.argv.slice(2), {
-    region: {
-        default: "us-east-2",
-        env: "AWS_GET_SECRET_REGION"
-    },
-    role: {
-        default: "",
-        env: "AWS_GET_SECRET_ROLE"
-    },
-    timeout: {
-        default: 5000,
-        env: "AWS_GET_SECRET_TIMEOUT"
-    },
-    ['session-name']: {
-        default: "param_session",
-        env: "AWS_GET_SECRET_SESSION_NAME"
-    },
-    verbose: {
-        default: false,
-        env: "AWS_GET_SECRET_VERBOSE"
-    },
+  region: {
+    default: "us-east-2",
+    env: "AWS_GET_SECRET_REGION",
+  },
+  role: {
+    default: "",
+    env: "AWS_GET_SECRET_ROLE",
+  },
+  timeout: {
+    default: 5000,
+    env: "AWS_GET_SECRET_TIMEOUT",
+  },
+  ["session-name"]: {
+    default: "param_session",
+    env: "AWS_GET_SECRET_SESSION_NAME",
+  },
+  verbose: {
+    default: false,
+    env: "AWS_GET_SECRET_VERBOSE",
+  },
 });
 
 (async function run() {
+  try {
     if (args.verbose) {
-        console.log("aws-get-secret", { options: args });
+      console.log("aws-get-secret", { options: args });
     }
 
     // Prepare client
@@ -40,20 +41,21 @@ const [args, remainder] = parse(process.argv.slice(2), {
     // Update environment
     const secretEnvs = findAll(process.env);
     if (args.verbose) {
-        console.log("refs", secretEnvs);
+      console.log("refs", secretEnvs);
     }
     const lookups = secretEnvs.map((envSecret) => envSecret.handle(source));
-    const env = await Promise.all(lookups).then(list => Object.fromEntries(list.filter(isDefined)));
+    const env = await Promise.all(lookups).then((list) =>
+      Object.fromEntries(list.filter(isDefined))
+    );
 
     // Run delegate command
-    try {
-        execSync(remainder, { env: { ...process.env, ...env }  });
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
-    }
+    execSync(remainder, { env: { ...process.env, ...env } });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 })();
 
 function isDefined<T>(v: T | undefined): v is T {
-    return typeof v !== "undefined";
+  return typeof v !== "undefined";
 }
